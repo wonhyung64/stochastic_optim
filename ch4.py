8d5624d4daca6ace9615d88dac62f96b92c61bae
WonHyung Shin <wonhyung64@icloud.com>
2023-10-05 13:21:27 +0900

    feat - solving homework2

---
+++ ch4.py
@@ -0,0 +1,123 @@
+#%%
+import numpy as np
+
+
+## Ex) 4.2
+#%%
+def init_theta():
+    return np.ones([2])
+
+
+def compute_grad(theta):
+    t1, t2 = theta
+    grad_t1 = 4 * np.power(t1, 3) + 2 * t1 + t2
+    grad_t2 = t1 + 2 * t2
+    return np.array(grad_t1, grad_t2)
+
+
+def compute_loss(theta):
+    t1, t2 = theta
+    return np.power(t1, 4), np.power(t1, 2), + t1 * t2 + np.power(t2, 2)
+
+
+def generate_noise(sigma):
+    return np.random.multivariate_normal(np.zeros([2]), np.identity(2) * sigma**2)
+
+
+def generate_gain_seq(iter, power_factor):
+    return 0.1 / np.power(iter + 1, power_factor)
+
+
+def update_theta(theta, grad, gain_seq):
+    return theta - gain_seq * grad
+
+
+max_iter = 1000
+max_repl = 200
+power_factor_list = [1., .501]
+sigma_list = [0.1, 1.0]
+
+for sigma in sigma_list:
+
+    for power_factor in power_factor_list:
+        results = []
+
+        for seed in range(max_repl):
+            np.random.seed(seed)
+            theta = init_theta()
+
+            for iter in range(1, max_iter+1): 
+                noise = generate_noise(sigma)
+                grad = compute_grad(theta) + noise
+                gain_seq = generate_gain_seq(iter, power_factor)
+                theta = update_theta(theta, grad, gain_seq)
+
+            terminal_loss = compute_loss(theta)
+            results.append(terminal_loss)
+        
+        print(f"[Sigma: {sigma} / Gain seq: {power_factor}] mean terminal loss: {np.round(np.mean(results), 5)}")
+
+
+## Ex) 4.5
+#%%
+def generate_x_pairs(max_sample_num=1000):
+    c = np.random.randint(1, 11, size=max_sample_num)
+    w = np.random.randint(11, 111, size=max_sample_num)
+    return np.stack([c,w], 1)
+
+def init_theta():
+    return np.array([1., .5])
+
+
+def generate_gain_seq(iter):
+    return 0.0015 / np.power(iter + 100, .501) 
+
+
+def generate_noise_1():
+    return np.random.normal(0, 5**2)
+
+
+def generate_noise_2(optimal_theta, x):
+    h = compute_h(optimal_theta, x)
+    w = np.random.normal(0, 1)
+    return 0.2 * h * w
+
+
+def compute_h(theta, x):
+    lmbd, beta = theta
+    c, w = x
+    return lmbd * np.power(c, beta) * np.power(w, 1-beta)
+
+
+def compute_euclidian(theta, optimal_theta):
+    return np.sqrt(np.sum(np.power(theta - optimal_theta, 2)))
+
+
+max_iter = 1000
+max_repl = 5
+optimal_theta = np.array([2.5, 0.7])
+
+x = generate_x_pairs()
+for noisy_type in ["Independent", "Dependent"]:
+
+    for seed in range(max_repl):
+        np.random.seed(seed)
+        np.random.shuffle(x)
+        
+        for iter, x_sample in enumerate(x):
+            gain_seq = generate_gain_seq(iter+1)
+            x_sample
+            if noisy_type == "Independent":
+                noise = generate_noise_1()
+            else:
+                noise = generate_noise_2(optimal_theta, x_sample)
+
+            z = compute_h(theta, x_sample) + noise
+            
+
+
+## Ex) 4.10
+#%%
+
+## Ex) 4.15
+#%%
